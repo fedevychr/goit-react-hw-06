95ab700fcfcb0a15b7d2c30b51352651b9a9bc86
Roman Fedevych <fedevychr@gmail.com>
2024-05-18 16:51:41 +0300

    add persistor to main and store

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
 // import "modern-normalize";
 import App from "./App.jsx";
 import "./index.css";
@@ -10,7 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
